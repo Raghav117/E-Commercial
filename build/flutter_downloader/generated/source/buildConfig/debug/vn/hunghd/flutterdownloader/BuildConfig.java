78a376ca91bd3146cb43bfb28b3319ed8098f162
Raghav Garg <42090624+Raghav117@users.noreply.github.com>
2021-07-20 12:29:45 +0530

    module 2 doing

---
+++ build/flutter_downloader/generated/source/buildConfig/debug/vn/hunghd/flutterdownloader/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package vn.hunghd.flutterdownloader;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "vn.hunghd.flutterdownloader";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
